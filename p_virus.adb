33a2252618d360f1d2b9018e8b4b425ec1292111
loic <lpiernas.pers@gmail.com>
2021-02-01 10:18:09 +0100

    edit p_virus.adb

---
+++ p_virus.adb
@@ -0,0 +1,11 @@
+
+package body p_virus is
+  procedure InitPartie(Grille : in out TV_Grille; Pieces : in out TV_Pieces);
+	-- {} => {Tous les éléments de Grille ont été initialisés avec la couleur VIDE, y compris les cases inutilisables
+	--				Tous les élements de Pieces ont été initialisés à false}
+
+  begin
+
+  end InitPartie;
+
+end p_virus;
