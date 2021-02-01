f67a0e8a214cd07d457f17dfb0eb713bed6e312c
loic <lpiernas.pers@gmail.com>
2021-02-01 13:36:08 +0100

    V3

--- p_virus.adb
+++ p_virus.adb
@@ -85,5 +85,101 @@ package body p_virus is
     return (Grille(1,'A') = rouge) and (Grille(2,'B') = rouge);
    end Guerison;
 
+   procedure MajGrille(Grille : in out TV_Grille; coul : in T_CoulP; Dir : in T_Direction) is
+  tampon: TV_Grille := Grille; -- Je sais plus pk il le faut mais pour eviter de deplacer des truc deja deplacés je crois
+  begin
+  for y in T_lig'range loop
+      for x in T_col'range loop
+        if Grille(y, x) = coul then
+          Grille(y, x) := vide; -- vide l'ancienne case
+          case Dir is
+            when bg => tampon(T_lig'pred(y), T_col'pred(x)):= coul; -- pas besoin de faire de check suplementaires car le mouvement et cencé etre possible
+            when bd => tampon(T_lig'pred(y), T_col'succ(x)) := coul;
+            when hg => tampon(T_lig'succ(y), T_col'pred(x)) := coul;
+            when hd => tampon(T_lig'succ(y), T_col'succ(x)) := coul;
+          end case;
+        end if;
+      end loop;
+    end loop;
+    for y in T_lig'range loop
+      for x in T_col'range loop
+        if tampon(y, x) = coul then
+          Grille(y,x) := coul; -- copie les nouveau deplacement dans la grille
+        end if;
+      end loop;
+    end loop;
+  end MajGrille;
+
+
+
+  function Possible(Grille : in TV_Grille; coul : in T_CoulP; Dir : in T_Direction) return boolean is
+
+  begin
+
+  for i in T_lig'range loop
+    for j in T_col'range loop
+
+      if Grille(i,j) = coul then
+
+        if Dir = bg then
+
+          if i /= T_lig'first and then j /= T_col'first then --Check si c'est pas les premiers pour sortir de la range
+
+            if Grille(T_lig'pred(i), T_col'pred(j)) /= vide then
+              return false;
+            end if;
+
+          else
+            return false; --Si on sort de la range, on peut pas déplacer donc false
+          end if;
+
+
+        elsif Dir = hg then
+          if i /= T_lig'last and then j /= T_col'first then
+
+            if Grille(T_lig'succ(i), T_col'pred(j)) /= vide then
+              return false;
+            end if;
+
+          else
+            return false; --Si on sort de la range, on peut pas déplacer donc false
+          end if;
+
+
+        elsif Dir = bd then
+          if i /= T_lig'first and then j /= T_col'last then
+
+            if Grille(T_lig'pred(i), T_col'succ(j)) /= vide then
+              return false;
+            end if;
+
+          else
+            return false; --Si on sort de la range, on peut pas déplacer donc false
+          end if;
+
+
+        elsif Dir = hd then
+          if i /= T_lig'last and then j /= T_col'last then
+
+            if Grille(T_lig'succ(i), T_col'succ(j)) /= vide then
+              return false;
+            end if;
+
+          else
+            return false; --Si on sort de la range, on peut pas déplacer donc false
+          end if;
+
+
+        end if;
+      end if;
+
+    end loop;
+  end loop;
+
+  --Si on a verifié les mouvement et que y'a rien qui bloque, on return true
+  return true;
+
+  end Possible;
+
 
 end p_virus;
