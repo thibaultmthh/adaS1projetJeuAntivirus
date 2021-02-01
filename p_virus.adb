03d8d788739c57f6dccd7a385527be11f588f552
Thibault <thibault.mathian@free.fr>
2021-02-01 10:54:34 +0100

    Guerison Done th

--- p_virus.adb
+++ p_virus.adb
@@ -1,6 +1,7 @@
 
 package body p_virus is
-  procedure InitPartie(Grille : in out TV_Grille; Pieces : in out TV_Pieces);
+
+  procedure InitPartie(Grille : in out TV_Grille; Pieces : in out TV_Pieces) is
 	-- {} => {Tous les éléments de Grille ont été initialisés avec la couleur VIDE, y compris les cases inutilisables
 	--				Tous les élements de Pieces ont été initialisés à false}
 
@@ -17,6 +18,12 @@ package body p_virus is
     end loop;
     
   end InitPartie;
+
+
+	function Guerison(Grille : in TV_Grille) return boolean is
+   begin 
+    return (Grille(1,'A') = rouge) and (Grille(2,'B') = rouge);
+   end Guerison;
 
 
 end p_virus;
