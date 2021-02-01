ae067defaddebb01089a9d683bad3a32d263ca87
nico.vrn <networksgamesni@gmail.com>
2021-02-01 13:19:11 +0100

    testjeu

--- testjeu.adb
+++ testjeu.adb
@@ -1,5 +1,6 @@
 with p_esiut; use p_esiut;
 with p_virus; use p_virus; use p_virus.p_piece_io;
+
 procedure testjeu is
   numdef : integer range 1..20;
   Grille : TV_Grille;
@@ -8,8 +9,8 @@ procedure testjeu is
 begin
   InitPartie(Grille, Pieces);
   open(f, in_file, "Defis.bin");
-  ecrire_ligne("entrez un numéro de défi:");
+  ecrire_ligne("entrez un numéro de défi entre 1 et 20:");
   lire(numdef);
   Configurer(f, numdef, Grille, Pieces);
-  pospiece(grille,vert);
+  pospiece(grille,jaune);
 end testjeu;
