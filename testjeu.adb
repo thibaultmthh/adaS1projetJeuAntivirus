59d7ecdf5796a29e9e6412b366fa62f6f4935b10
Nico <45742039+nico-vrn@users.noreply.github.com>
2021-02-02 10:44:33 +0100

    Update testjeu.adb

--- testjeu.adb
+++ testjeu.adb
@@ -78,10 +78,10 @@ begin
       if Guerison(grille) then
         ecrire_ligne("TU AS GAGNE en" & image(compteur_mouvement) &" GG !! veux-tu rejouer? (y ou n)");
         lire(rejouer);
-        if rejouer="y" then
+        if rejouer='y' then
           ecrire_ligne("c'est reparti");
           exit;
-        elsif rejouer="n" then
+        elsif rejouer='n' then
           ecrire_ligne("j'espère que tu reviendra bientôt");
           stop:=true;
           exit;
