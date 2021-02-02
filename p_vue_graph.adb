c20e2b05470ccfff981fab6e0490874a858da7df
loic <lpiernas.pers@gmail.com>
2021-02-02 17:02:47 +0100

    p_vuegra

---
+++ p_vue_graph.adb
@@ -0,0 +1,6 @@
+with p_fenbase, forms, p_esiut;
+use  p_fenbase, forms, p_esiut;
+
+package body p_vuegraph is
+
+end p_vuegraph;
--- p_vue_graph.ads
+++ p_vue_graph.ads
@@ -2,7 +2,8 @@ with p_fenbase, forms, p_esiut;
 use  p_fenbase, forms, p_esiut;
 
 package p_vuegraph is
-  procedure InitFenetreprincipale
+  procedure InitFenetreprincipale(fenetre : out TR_Fenetre);
+
 
 
 
