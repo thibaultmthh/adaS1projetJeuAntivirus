405467dbab12244f05d103dea1281d224cd5d928
Thibault M <thibault.mathian@free.fr>
2021-02-03 14:30:36 +0100

    format av_graph

--- av_graph.adb
+++ av_graph.adb
@@ -1,42 +1,44 @@
 with p_fenbase, forms, p_esiut, p_vue_graph;
-use  p_fenbase, forms, p_esiut, p_vue_graph;
-
+use p_fenbase, forms, p_esiut, p_vue_graph;
 
 procedure av_graph is
   fprincipale : TR_Fenetre;
-  fpseudo : TR_Fenetre;
-  continue : Character;
-  pseudo : string(1..3);
+  fpseudo     : TR_Fenetre;
+  continue    : Character;
+  pseudo      : String (1 .. 3);
 begin -- av_graph
   InitialiserFenetres;
-  initfenetrepseudo(fpseudo);
+  initfenetrepseudo (fpseudo);
 
-  MontrerFenetre(fpseudo);
-  ChangerTempsMinuteur(fprincipale, "Chronometre", 200000.0);
+  MontrerFenetre (fpseudo);
+  ChangerTempsMinuteur (fprincipale, "Chronometre", 200_000.0);
   -- ChangerMinuteurEnChrono(fprincipale, "Chronometre");
 
   declare
-    Bouton : String := (Attendrebouton(fpseudo));
-    begin
-      if bouton = "quitter" then
-        CacherFenetre(fpseudo);
-      elsif bouton = "jouer" then
-        pseudo:=Consultercontenu(fpseudo,"pseudo");
-        CacherFenetre(fpseudo);
-        InitFenetreprincipale(fprincipale, pseudo);
-        MontrerFenetre(fprincipale);
-        declare
-          Bouton : String := (Attendrebouton(fprincipale));
-        begin
-          if bouton = "quitter" then
-            CacherFenetre(fprincipale);
-          end if;
-        end;
-      end if;
-    end;
+    Bouton : String := (Attendrebouton (fpseudo));
+  begin
+    if Bouton = "quitter" then
+      CacherFenetre (fpseudo);
+    elsif Bouton = "jouer" then
+      pseudo := Consultercontenu (fpseudo, "pseudo");
+      CacherFenetre (fpseudo);
+      InitFenetreprincipale (fprincipale, pseudo);
+      MontrerFenetre (fprincipale);
 
-  ecrire_ligne(ConsulterTimer(fprincipale,"Chronometre"));        --nom de la fenêtreNomElement : inString      )
-  ecrire_ligne("fin");
+    end if;
+  end;
+  declare
+    Bouton : String := (Attendrebouton (fprincipale));
+  begin
+    if Bouton = "quitter" then
+      CacherFenetre (fprincipale);
+    end if;
+  end;
 
+  ecrire_ligne
+   (ConsulterTimer
+     (fprincipale,
+      "Chronometre"));        --nom de la fenêtreNomElement : inString      )
+  ECRIRE_LIGNE ("fin");
 
 end av_graph;
