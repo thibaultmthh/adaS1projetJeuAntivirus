6dfeffc3a741b019634426b5cd56af20fcf50987
nico-vrn <networksgamesni@gmail.com>
2021-02-03 14:48:59 +0100

    push bouton quitter

--- av_graph.adb
+++ av_graph.adb
@@ -24,21 +24,15 @@ begin -- av_graph
       CacherFenetre (fpseudo);
       InitFenetreprincipale (fprincipale, pseudo);
       MontrerFenetre (fprincipale);
-
+        declare
+          Bouton : String := (Attendrebouton (fprincipale));
+        begin
+          if Bouton = "quitter" then
+            CacherFenetre (fprincipale);
+          end if;
+        end;
     end if;
   end;
-  loop
-    declare
-      Bouton : String := (Attendrebouton (fprincipale));
-    begin
-      if Bouton = "quitter" then
-        CacherFenetre (fprincipale);
-        exit;
-      end if;
-
-    end;
-
-  end loop;
 
   ecrire_ligne
    (ConsulterTimer
