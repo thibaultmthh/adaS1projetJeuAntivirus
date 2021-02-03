315bee95965672682ad211639e0dc4972e7020a2
nico-vrn <networksgamesni@gmail.com>
2021-02-03 15:03:56 +0100

    push rickroll

--- p_vue_graph.adb
+++ p_vue_graph.adb
@@ -107,7 +107,7 @@ package body p_vue_graph is
 
     AjouterTexte( fenetre, "Bienvenue" , "Bienvenue au Jeu ANTI VIRUS!!" , 200, 20, 200, 50);
     AjouterImage ( fenetre , "imageAntiVirus" , "antivirusimage.xpm" ,"" , 200 , 150, 200 , 200 );
-    AjouterImage ( fenetre , "imageTousAntiCovid" , "anticovid.xpm" , "" , 0 , 428 , 77,231) ;
+    AjouterImage ( fenetre , "imageTousAntiCovid" , "anticovid.xpm" , "" , 0 , 407 , 107,231) ;
 
     FinFenetre (fenetre);
   end initfenetrepseudo;
